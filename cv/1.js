cf757159329cac75bc9a31da2cb40d58f3ecd6b8
thang <hcthangdev@gmail.com>
2021-02-23 11:30:18 +0700

    sua vi tri comments va menu

--- cv/1.js
+++ cv/1.js
@@ -1,6 +1,25 @@
 ﻿ $(function(){
+    // scroll to top
+    $('.to-top').click(() =>{
+        $('body, html').animate({scrollTop: 0}, 800);
+        return false
+    })
+    
+    // scroll to exp
     $('.computer-mouse, .to-exp').click(() =>{
         $('body, html').animate({scrollTop: $('#experience-and-lerning-progress').offset().top}, 800);
+        return false
+    })
+    
+    // scroll to exp
+    $('.to-contact').click(() =>{
+        $('body, html').animate({scrollTop: $('#contact').offset().top}, 800);
+        return false
+    })
+    
+    // scroll to exp
+    $('.to-comments').click(() =>{
+        $('body, html').animate({scrollTop: $('#comments').offset().top}, 800);
         return false
     })
 })  
