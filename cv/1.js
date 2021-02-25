4e7e9989a48996eece10d803e91a1eaebb3fac3e
thang <hcthang23532@gmail.com>
2021-02-25 10:06:11 +0700

    fix menu

--- cv/1.js
+++ cv/1.js
@@ -1,26 +1,29 @@
-﻿ $(function(){
+﻿$(function() {
+    function heightMenu() {
+        return document.getElementById('menu').clientHeight
+    }
+
     // scroll to top
-    $('.to-top').click(() =>{
-        $('body, html').animate({scrollTop: 0}, 800);
+    $('.to-top').click(() => {
+        $('body, html').animate({ scrollTop: 0 }, 800);
         return false
     })
-    
+
     // scroll to exp
-    $('.computer-mouse, .to-exp').click(() =>{
-        $('body, html').animate({scrollTop: $('#experience-and-lerning-progress').offset().top}, 800);
+    $('.computer-mouse, .to-exp').click(() => {
+        $('body, html').animate({ scrollTop: $('#experience-and-lerning-progress').offset().top - heightMenu() }, 800);
         return false
     })
-    
+
     // scroll to exp
-    $('.to-contact').click(() =>{
-        $('body, html').animate({scrollTop: $('#contact').offset().top}, 800);
+    $('.to-contact').click(() => {
+        $('body, html').animate({ scrollTop: $('#contact').offset().top - heightMenu() }, 800);
         return false
     })
-    
+
     // scroll to exp
-    $('.to-comments').click(() =>{
-        $('body, html').animate({scrollTop: $('#comments').offset().top}, 800);
+    $('.to-comments').click(() => {
+        $('body, html').animate({ scrollTop: $('#comments').offset().top - heightMenu() }, 800);
         return false
     })
-})  
- 
+})
