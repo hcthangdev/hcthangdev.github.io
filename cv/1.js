109e39889421a9bc5e4a562c0260fd8e11fdd148
thang <hcthang23532@gmail.com>
2021-03-17 16:48:25 +0700

    fix cv

--- cv/1.js
+++ cv/1.js
@@ -3,7 +3,8 @@
         return document.getElementById('menu').clientHeight
     }
 
-    // scroll to top
+    transition = 800
+        // scroll to top
     $('.to-top').click(() => {
         $('body, html').animate({ scrollTop: 0 }, 800);
         return false
@@ -11,19 +12,19 @@
 
     // scroll to exp
     $('.computer-mouse, .to-exp').click(() => {
-        $('body, html').animate({ scrollTop: $('#experience-and-lerning-progress').offset().top - heightMenu() }, 800);
+        $('body, html').animate({ scrollTop: $('#experience-and-lerning-progress').offset().top - heightMenu() }, transition);
         return false
     })
 
     // scroll to exp
     $('.to-contact').click(() => {
-        $('body, html').animate({ scrollTop: $('#contact').offset().top - heightMenu() }, 800);
+        $('body, html').animate({ scrollTop: $('#contact').offset().top - heightMenu() }, transition);
         return false
     })
 
     // scroll to exp
     $('.to-comments').click(() => {
-        $('body, html').animate({ scrollTop: $('#comments').offset().top - heightMenu() }, 800);
+        $('body, html').animate({ scrollTop: $('#comments').offset().top - heightMenu() }, transition);
         return false
     })
 })
